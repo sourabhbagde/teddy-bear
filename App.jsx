94e9c3f904005de185e2b75f7cdf0a7c138d3924
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-09-30 02:31:36 -0500

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const App = () => {
+  return <div>Hello World</div>;
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+export default App;
